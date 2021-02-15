4e58727fc006fb314b1606a5de15f343e872aabe
Allyson <allysonlarkindesign@gmail.com>
2021-02-15 11:00:39 +0100

    Updated spacing in GitHub link

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App (){
   return (
   <div className = "App">
     <h1>Weather App</h1>
-    <footer><a href="https://github.com/alarkin09/react-final-weather-app" target="_blank">Open source code{" "}</a>by Allyson Larkin
+    <footer><a href="https://github.com/alarkin09/react-final-weather-app" target="_blank">Open source code</a>{" "}by Allyson Larkin
     </footer>
   </div>
   );
