785fcb6aecfc4f9e7ecc8bae2c3247c0426c928f
Allyson <allysonlarkindesign@gmail.com>
2021-02-15 13:29:42 +0100

    Added search form and button

--- src/Weather.js
+++ src/Weather.js
@@ -3,6 +3,10 @@ import "./Weather.css";
 
 export  default function Weather (){
     return (<div className="Weather">
+        <form>
+            <input type="search" placeholder="Search city here" className="form-control"/>
+            <input type="submit" value="Search" className="btn btn-primary"/>
+        </form>
         <h1>Geneva</h1>
         <ul>
             <li>Monday 12:00</li>
@@ -13,13 +17,13 @@ export  default function Weather (){
                 <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="Partly Sunny"/>
                 7° C
             </div>
-        </div>
-        <div className="col-6">
+            <div className="col-6">
             <ul>
                 <li>Precipitation</li>
                 <li>Humidity</li>
                 <li>Wind</li>
             </ul>
+            </div>
         </div>
         </div>);
 }
