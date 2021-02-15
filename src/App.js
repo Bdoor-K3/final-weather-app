8649afe8d2d00411858cc7885488f7648d375fb8
Allyson <allysonlarkindesign@gmail.com>
2021-02-15 10:59:13 +0100

    Added link to GitHub

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 
 export default function App (){
   return (
-  <div className = "App">Weather App
+  <div className = "App">
+    <h1>Weather App</h1>
+    <footer><a href="https://github.com/alarkin09/react-final-weather-app" target="_blank">Open source code{" "}</a>by Allyson Larkin
+    </footer>
   </div>
   );
 }
