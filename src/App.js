4738a203408e93aeb938ca1f916788a1ea9c5144
Allyson <allysonlarkindesign@gmail.com>
2021-02-16 11:49:49 +0100

    CSS: added typeface and color palette

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ export default function App (){
   return (
   <div className = "App">
     <div className = "container">
-    <h1>Weather App</h1>
     <Weather/>
     <footer><a href="https://github.com/alarkin09/react-final-weather-app" target="_blank">Open source code</a>{" "}by Allyson Larkin
     </footer>
--- src/Weather.js
+++ src/Weather.js
@@ -1,12 +1,32 @@
 import React from "react";
 import "./Weather.css";
+import axios from "axios";
 
 export  default function Weather (){
+    const apiKey="29a44231d6c7563fcc1740ef29d751ed";
+    let city = "Geneva"
+    let apiUrl= `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    
+    function handleResponse(response){
+        console.log(response.data);
+    }
+    axios.get(apiUrl).then(handleResponse);
+
+
+
     return (<div className="Weather">
+        
         <form>
+            <div className="row">
+                <div className="col-5">
             <input type="search" placeholder="Search city here" className="form-control"/>
+            </div>
+            <div className="col-3">
             <input type="submit" value="Search" className="btn btn-primary"/>
+            </div>
+        </div>
         </form>
+        
         <h1>Geneva</h1>
         <ul>
             <li>Monday 12:00</li>
