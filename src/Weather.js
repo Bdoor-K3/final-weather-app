c033ce28c5fde020af974c2173abaa042649649f
Allyson <allysonlarkindesign@gmail.com>
2021-02-16 14:10:22 +0100

    CSS Updates: temp, unit, image, color

--- src/Weather.js
+++ src/Weather.js
@@ -14,15 +14,16 @@ export  default function Weather (){
 
 
 
-    return (<div className="Weather">
+    return (
+    <div className="Weather">
         
         <form>
             <div className="row">
-                <div className="col-5">
-            <input type="search" placeholder="Search city here" className="form-control"/>
+                <div className="col-9">
+            <input type="search" placeholder="Search city here" className="form-control" autoFocus="on"/>
             </div>
             <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary"/>
+            <input type="submit" value="Search" className="btn btn-primary w-100"/>
             </div>
         </div>
         </form>
@@ -32,11 +33,16 @@ export  default function Weather (){
             <li>Monday 12:00</li>
             <li>Partly Sunny</li>
         </ul>
-        <div className="row">
+        <div className="row mt-3" >
             <div className="col-6">
-                <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="Partly Sunny"/>
-                7° C
-            </div>
+                <div className="clearfix">
+                <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="Partly Sunny" className="float-left"/>
+                <div className="float-left">
+                <span className="temperature">7</span>
+                <span className="unit">°C</span>
+                </div>
+                </div>
+                </div>
             <div className="col-6">
             <ul>
                 <li>Precipitation</li>
@@ -44,6 +50,7 @@ export  default function Weather (){
                 <li>Wind</li>
             </ul>
             </div>
-        </div>
-        </div>);
+            </div>
+       </div>
+      );
 }
